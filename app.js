fa60a12bca6215120197807bf94fe3a631324338
Jonathan <jonathangomezit@gmail.com>
2025-02-24 13:21:51 -0300

    menos insultos

--- app.js
+++ app.js
@@ -5,57 +5,59 @@ const canvas = document.getElementById("canvas");
 const contentSVG = document.querySelector(".content");
 const press = document.querySelector(".press");
 const QR = ({ value }) => {
-  return new Promise((resolve, reject) => {
-    QRCode.toString(
-      value,
-      {
-        width: 500,
-        height: 500,
-        errorCorrectionLevel: "H",
-        type: "svg",
-        margin: 0,
-      },
-      (e, str) => {
-        if (e) {
-          reject(e);
-          return;
-        }
-        resolve(str);
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        QRCode.toString(
+            value,
+            {
+                width: 500,
+                height: 500,
+                errorCorrectionLevel: "H",
+                type: "svg",
+                margin: 0,
+            },
+            (e, str) => {
+                if (e) {
+                    reject(e);
+                    return;
+                }
+                resolve(str);
+            }
+        );
+    });
 };
 
 form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const input = e.target.querySelector('input[type="text"]');
-  const value = input.value;
+    e.preventDefault();
+    const input = e.target.querySelector('input[type="text"]');
+    const value = input.value;
 
-  QR({ canvas, value })
-    .then((e) => {
-      input.value = "";
-      contentSVG.innerHTML = "";
-      const p = document.createElement("p");
-      p.textContent = value;
-      const source = '<?xml version="1.0" standalone="no"?>\r\n' + e;
-      const url =
-        "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
-      const node = `<a download="qr.svg" href='${url}' >${e.replace(
-        /width="500" height="500"/,
-        ""
-      )}</a>`;
+    QR({ canvas, value })
+        .then((e) => {
+            input.value = "";
+            contentSVG.innerHTML = "";
+            const p = document.createElement("p");
+            p.textContent = value;
+            const source = '<?xml version="1.0" standalone="no"?>\r\n' + e;
+            const url =
+                "data:image/svg+xml;charset=utf-8," +
+                encodeURIComponent(source);
+            const node = `<a download="qr.svg" href='${url}' >${e.replace(
+                /width="500" height="500"/,
+                ""
+            )}</a>`;
 
-      const rango = document.createRange();
-      const fr = rango.createContextualFragment(node);
+            const rango = document.createRange();
+            const fr = rango.createContextualFragment(node);
 
-      contentSVG.appendChild(fr);
-      press.style.display = "block";
-      fragment.appendChild(p);
-      list.insertBefore(fragment, list.firstChild);
-    })
-    .catch((e) => {
-      alert("No escribiste nada pelotudo/a");
-      console.log(e);
-      input.focus();
-    });
+            contentSVG.appendChild(fr);
+            press.style.display = "block";
+
+            fragment.appendChild(p);
+            list.insertBefore(fragment, list.firstChild);
+        })
+        .catch((e) => {
+            alert("No escribiste nada");
+            console.log(e);
+            input.focus();
+        });
 });
